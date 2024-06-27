947250d563789a98f48b9bc61ee03b51837f2c53
dingrui <Bannirui@outlook.com>
2024-06-27 18:09:37 +0800

    有效数字

--- tests/01/main.c
+++ tests/01/main.c
@@ -10,35 +10,38 @@
 int main(int argc, char** argv)
 {
 	char str[MAXSIZE];
-	// get a line from a stream
-	gets(str);
 	int i;
-	// 分别记录每个C之前的A的数量和之后M的数量 ACM的数量
-	int count_a=0, count_m=0, count=0;
-	// 所有M的数量
-	for(i=0;str[i]!='\0';i++)
+	for (i = 0; argv[1][i] != '\0'; i++)
 	{
-		if(str[i]=='M')
+		str[i] = argv[1][i];
+	}
+	printf("样本: %s\n", str);
+	// 分别记录每个C之前的A的数量和之后M的数量 ACM的数量
+	int count_a = 0, count_m = 0, count = 0;
+	// 所有M的数量
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 'M')
 		{
 			count_m++;
 		}
 	}
 	// 每遇到C就开始结算
-	for(i=0;str[i]!='\0';i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if(str[i]=='M')
+		if (str[i] == 'M')
 		{
 			count_m--;
 		}
-		else if(str[i]=='A')
+		else if (str[i] == 'A')
 		{
 			count_a++;
 		}
-		else if(str[i]=='C')
+		else if (str[i] == 'C')
 		{
-			count+=count_a*count_m;
+			count += count_a * count_m;
 		}
 	}
-	printf("有%d个ACM\n", count);
+	printf("结果: 有%d个ACM\n", count);
 	return 0;
 }
--- tests/02/main.c
+++ tests/02/main.c
@@ -1,6 +1,108 @@
 #include<stdio.h>
+#include<math.h>
+
+/**
+ * 读取所有的数据 依次判断是否为非法数字 最终对所有合法数字求平均值
+ */
+
+// 单个数字最大长度
+#define MAX_SIZE 10
 
 int main(int argc, char** argv)
 {
+	int n;
+	sscanf(argv[1], "%d", &n);
+	if (n <= 0)
+	{
+		printf("参数必须>0");
+		return -1;
+	}
+	// 字符串数组
+	char str[n][MAX_SIZE];
+	for (int i = 0; i < n; i++)
+	{
+		// 读取每个字符串
+		sscanf(argv[i + 2], "%s", str[i]);
+	}
+	float sum = 0;
+	int cnt = 0;
+	for (int i = 0; i < n; i++)
+	{
+		// 字符串转换为数字
+		float num = 0;
+		/**
+		 * 数字是否合法标识
+		 * 0标识非法数字
+		 * 1标识当前数字为整数
+		 * 2标识当前数字为小数
+		 */
+		int flag = 1;
+		// 当前数字是小数 记录小数点后有几位
+		int precision = 0;
+		// 标识数字的正负 1标识当前数字是负数 0标识当前数字是正数
+		int negative = 0;
+		// 轮询每个字符
+		char ch;
+		for (int j = 0; (ch = str[i][j]) != '\0'; j++)
+		{
+			if (ch >= '0' && ch <= '9')
+			{
+				if (flag == 1)
+				{
+					// 整数 需要分正负
+					num = num * 10;
+					if (negative)
+					{
+						num -= (ch - '0');
+					}
+					else
+					{
+						num += (ch - '0');
+					}
+				}
+				else if (flag == 2 && precision < 2)
+				{
+					// 小数
+					num += pow(10, -1 * (++precision)) * (ch - '0');
+				}
+				else
+				{
+					flag = 0;
+					break;
+				}
+			}
+			else if (str[i][j] == '-' && !negative)
+			{
+				// 负数
+				negative = 1;
+			}
+			else if (str[i][j] == '.' && flag == 1)
+			{
+				// 遇到了小数点 标识当前数字是小数
+				flag = 2;
+				precision = 0;
+			}
+			else
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag)
+		{
+			// 合法数字求和
+			sum += num;
+			printf("flag:%s, num:%.2f sum:%.2f\n", flag == 1 ? "整数" : flag == 2 ? "小数" : "未知", num, sum);
+			cnt++;
+		}
+	}
+	if (cnt > 0)
+	{
+		printf("结果: %.2f\n", sum / cnt);
+	}
+	else
+	{
+		printf("结果: %.2f\n", 0.0);
+	}
 	return 0;
 }
