e300be93b22e7b50d715ffd8e5847218f0d231fc
dingrui <Bannirui@outlook.com>
2024-07-08 16:03:05 +0800

    最小生成树

--- tests/08/MatrixUDG.cpp
+++ tests/08/MatrixUDG.cpp
@@ -11,7 +11,7 @@ MatrixUDG::MatrixUDG()
 	char c1, c2;
 	int i, j, weight, p1, p2;
 	// 顶点数和边数
-	std::cout << "input  vertex number: ";
+	std::cout << "input vertex number: ";
 	std::cin >> mVexNum;
 	std::cout << "input edge number: ";
 	std::cin >> mEdgNum;
@@ -26,7 +26,7 @@ MatrixUDG::MatrixUDG()
 		std::cout << "vertex(" << i << "): ";
 		mVexs[i] = readChar();
 	}
-	// 初始化边的权重
+	// 初始化边的权重 自己到自己的距离是0 其余顶点间的距离都初始化成最大值 然后等待输入的值来覆盖
 	for (i = 0; i < mVexNum; i++)
 	{
 		for (j = 0; j < mVexNum; j++)
@@ -37,21 +37,25 @@ MatrixUDG::MatrixUDG()
 				mMatrix[i][j] = INF;
 		}
 	}
-	// 根据输入进行初始化边的权重值
+	// 根据输入覆盖上面初始化的值
 	for (int i = 0; i < mEdgNum; i++)
 	{
 		// 读取边的起始顶点 结束顶点 权重值
 		std::cout << "edge(" << i << "): ";
 		c1 = readChar();
 		c2 = readChar();
+		// 顶点c1->c2这条边的权重值
 		std::cin >> weight;
+		// 顶点脚标位置
 		p1 = getPosition(c1);
+		// 顶点脚标位置
 		p2 = getPosition(c2);
 		if (p1 == -1 || p2 == -1)
 		{
 			std::cout << "input error: invalid edge" << std::endl;
 			return;
 		}
+		// 邻接矩阵中两个顶点间的距离
 		mMatrix[p1][p2] = weight;
 		mMatrix[p2][p1] = weight;
 	}
@@ -129,32 +133,41 @@ void MatrixUDG::BFS()
 void MatrixUDG::prim(int start)
 {
 	int min, i, j, k, m, n, sum;
+	// prims最小树的结果数组 形式如A->B->C->...这样的一个数组
+	char prims[MAX];
+	/**
+	 * 从start出发到各个顶点的距离
+	 * 0有2个语义
+	 * <ul>
+	 *   <li>顶点自己到自己的距离是0</li>
+	 *   <li>标识两个顶点已经处理过 加到了结果中</li>
+	 * </ul>
+	 */
+	int weights[MAX];
 	// prim最小树的索引 prims数组的索引
 	int index = 0;
-	// prims最小树的结果数组
-	char prims[MAX];
-	// 顶点间边的权重值
-	int weights[MAX];
-	// 第一个数是start顶点
+	// 整棵数的起点从顶点start开始
 	prims[index++] = mVexs[start];
-	// 初始化 第start个顶点到第i个顶点的权重
+	// 遍历所有顶点 缓存从顶点start到每一个顶点的距离
 	for (int i = 0; i < mVexNum; i++)
 	{
+		// 自己到自己的距离是0 从邻接表中读取start->i的距离
 		if (i == start) weights[i] = 0;
 		else weights[i] = mMatrix[start][i];
 	}
+	// 轮询所有顶点 每次处理的都是从轮询到的顶点为出发点 找距离最近的顶点
 	for (int i = 0; i < mVexNum; i++)
 	{
-		// 从start开始的 不需要再对第start个顶点进行处理
-		if (i == start)
-			continue;
+		// start是起始顶点 已经放到了结果中了 不需要对start进行处理
+		if (i == start) continue;
 		j = 0;
+		// k缓存最小距离的顶点编号 min缓存中最小距离
 		k = 0;
 		min = INF;
-		// 在未被加入到最小生成树的顶点中 找出权重值最小的顶点
+		// 从所有顶点中找到距离最小的目标顶点
 		while (j < mVexNum)
 		{
-			// 第j个节点已经排序过 加入到了最小生成树中
+			// 找到了最小距离的候选顶点 缓存起来
 			if (weights[j] != 0 && weights[j] < min)
 			{
 				min = weights[j];
@@ -162,14 +175,14 @@ void MatrixUDG::prim(int start)
 			}
 			j++;
 		}
-		// 经过上面的处理 在未被加入到最小生成树的顶点中 权重值最小的顶点是k 把顶点k加到最小生成树的结果数组中
+		// start->k是找到的最小的距离
 		prims[index++] = mVexs[k];
-		// 将k顶点的权重值标记为0 标识k顶点已经排序过了 加到了结果中
+		// 置0标识顶点k已经处理好了放在了结果中
 		weights[k] = 0;
 		// 当顶点k被加入到了最小生成树后 更新其他顶点权重值
 		for (j = 0; j < mVexNum; j++)
 		{
-			// 当节点j没有被处理 并且需要更新时才被更新
+			// 下一轮的距离 上一轮从start->k 下一轮从k->?
 			if (weights[j] != 0 && mMatrix[k][j] < weights[j])
 				weights[j] = mMatrix[k][j];
 		}
@@ -194,7 +207,7 @@ void MatrixUDG::prim(int start)
 	std::cout << "PRIM(" << mVexs[start] << ")=" << sum << ": ";
 	for (i = 0; i < index; i++)
 		std::cout << prims[i] << " ";
-	std::cout << std::endl;
+	std::cout << "\n";
 }
 void MatrixUDG::print()
 {
@@ -257,21 +270,26 @@ void MatrixUDG::dfs(int i, int* visited)
 
 int main()
 {
+	// 顶点
 	char vexs[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+	// 邻边
 	int matrix[][9] = {
-		{ 0, 12, INF, INF, INF, 16, 14 },
-		{ 12, 9, 10, INF, INF, 7, INF },
-		{ INF, 10, 0, 3, 5, 6, INF },
-		{ INF, INF, 3, 0, 4, INF, INF },
-		{ 16, 7, 6, INF, 2, 0, 9 },
-		{ 14, INF, INF, 8, 9, 0 }
+		//A     B    C    D     E   F     G
+		{ 0, 12, INF, INF, INF, 16, 14 }, // A
+		{ 12, 0, 9, 10, INF, INF, 7 },//  B
+		{ INF, 9, 0, 7, 5, 6, 8 },//  C
+		{ INF, 9, 7, 0, 4, INF, INF },// D
+		{ INF, INF, 5, 4, 0, INF, INF },// E
+		{ 16, INF, 6, INF, INF, 0, INF },//  F
+		{ 14, 7, 8, INF, INF, INF, 0 } //   G
 	};
+	// 顶点个数
 	int vlen = sizeof(vexs) / sizeof(vexs[0]);
 	MatrixUDG* pG;
 	pG = new MatrixUDG(vexs, vlen, matrix);
-	pG->print();
-	pG->DFS();
-	pG->BFS();
-	pG->prim(0);
+//	pG->print();
+//	pG->DFS();
+//	pG->BFS();
+	pG->prim(1);
 	return 0;
 }
--- tests/08/MatrixUDG.h
+++ tests/08/MatrixUDG.h
@@ -7,7 +7,7 @@
 
 class MatrixUDG
 {
-#define MAX 100
+#define MAX 1024
 #define INF (~(0x1<<31))
  private:
 	// 顶点集合
@@ -22,6 +22,12 @@ class MatrixUDG
  public:
 	// 创建图
 	MatrixUDG();
+	/**
+	 * 创建图
+	 * @param vexs 顶点
+	 * @param vlen 顶点个数
+	 * @param matrix 邻接矩阵
+	 */
 	MatrixUDG(char vexs[], int vlen, int matrix[][9]);
 	~MatrixUDG();
 
@@ -31,7 +37,7 @@ class MatrixUDG
 	void BFS();
 	/**
 	 * 从start开始生成最小生成树
-	 * @param start 从图中的第start个元素开始 生成最小树
+	 * @param start 顶点编号
 	 */
 	void prim(int start);
 	// 打印矩阵队列图
@@ -41,6 +47,11 @@ class MatrixUDG
 	// 读取一个输入字符
 	char readChar();
 	// 返回ch在mMatrix矩阵中的位置
+	/**
+	 * 获取顶点的位置
+	 * @param ch 顶点的名称
+	 * @return 顶点是个一维数组 返回顶点在数组中的脚标
+	 */
 	int getPosition(char ch);
 	// 返回顶点v的第一个邻接顶点的索引 失败则返回-1
 	int firstVertex(int v);
