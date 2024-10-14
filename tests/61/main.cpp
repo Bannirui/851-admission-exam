2cc82aaf54cbbbde07c9aaa9d32796c32732fcdf
dingrui <Bannirui@outlook.com>
2024-10-14 11:21:54 +0800

    递归分治 斐波那契数列

--- tests/61/main.cpp
+++ tests/61/main.cpp
@@ -16,9 +16,19 @@ int cal(int n) {
   return n * cal(n - 1);
 }
 
+int fab(int n) {
+  if (n == 0)
+    return 0;
+  if (n == 1)
+    return 1;
+  return fab(n - 1) + fab(n - 2);
+}
+
 int main(int argc, char **argv) {
 
   int n = 4;
-  std::cout << "递归计算 输入=" << n << ", 输出=" << cal(4) << std::endl;
+  std::cout << "递归 阶乘 输入=" << n << ", 输出=" << cal(4) << std::endl;
+  std::cout << "递归 斐波那契数列 输入=" << n << ", 输出=" << fab(n)
+            << std::endl;
   return 0;
 }
