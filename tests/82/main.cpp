c9553e798e06f12d38a3dbec97f7b7e23ee25d7e
dingrui <Bannirui@outlook.com>
2024-10-17 20:24:45 +0800

    tmp

---
+++ tests/82/main.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <utility>
+#include <valarray>
+#ifdef __linux__
+#include <malloc.h>
+#elif __APPLE__
+#include <sys/malloc.h>
+#endif
+
+#define maxSize 20
+
+void sort_vw(int n, float v[],float w[])
+{
+  float vw[maxSize],max;
+
+}
+
+int main(int argc, char **argv) {
+  std::cout << "hello world" << std::endl;
+  return 0;
+}
