85e31102f1bd00692c0f6588c16e183d7809c7b7
dingrui <Bannirui@outlook.com>
2024-11-12 10:53:34 +0800

    最小生成树Prim算法

--- tests/86/main.cpp
+++ tests/86/main.cpp
@@ -12,11 +12,13 @@
 #define MAX_SZ 20
 #define INF 1000
 
+// 顶点
 typedef struct VxNode
 {
   int no;
 } VexNode;
 
+// 图
 typedef struct MGraph
 {
   int edges[MAX_SZ][MAX_SZ];
@@ -43,50 +45,52 @@ int edgesW[5][5] =
  * 初始时所有结点都在v中 然后每一轮都从v中找到一个点 它到u中点的距离最小 把它从v中挪到u中
  * 直到v中所有结点都到了u中结束
  * @param g 图
- * @param v 选定的出发点
+ * @param start 选定的出发点
  * @param sum 以v为出发点的最小生成树的路径结果
  */
-void Prim(MGraph g, int v, int &sum)
+void Prim(MGraph g, int start, int &sum)
 {
-  // visit 用于标识顶点被访问过 1表示被访问过 0表示还没被访问
-  // u u集合
-  int visit[MAX_SZ], dist[MAX_SZ], u;
+  // u 集合 初始时u是空的
+  // dist[i]语义是集合u到v中v[i]的距离是最短的
+  int u[MAX_SZ], dist[MAX_SZ], tmp;
 
   for(int i=0;i<g.v;++i)
   {
-    // 点v到各个点的距离
-    dist[i]=g.edges[v][i];
-    // 初始化点都没被访问
-    visit[i]=0;
+    // 此时u中还是空的 准备把start加到u中 准备好v中所有结点到start的距离
+    dist[i]=g.edges[start][i];
+    // 初始化u 开始时u中是空的
+    u[i]=0;
   }
-  // 标记顶点v被访问过了
-  visit[v]=1;
-  std::cout<<"最小生成树的顶点为"<<v<<" ";
+  // 先把v点放到u中
+  // 最终结果是最小生成树 特点是有n个结点 n-1条边 因此总共经过n轮遍历
+  u[start]=1;
+  std::cout<<"最小生成树的顶点为"<<start<<" ";
 
-  // 总共n个顶点 出发点已经确定好 剩下来n-1个顶点
+  // u中已经放好了一个结点 还剩n-1个结点要放
 	for(int i=0;i<g.v-1;++i)
   {
+    // v中结点到u中结点距离最短的距离
 		int min=INF;
+    // 从集合v中找到一个跟集合u中最近的点 放到集合u中
 		for(int j=0;j<g.v;++j)
     {
-			if(!visit[j] && dist[j]<min)
+      // 结点不是在u中就是在v中 不在u中就一定在v中
+			if(!u[j] && dist[j]<min)
       {
 				min=dist[j];
-				u=j;
+				tmp=j;
 			}
     }
-    // 找到了距离v最近的点各距离 v->u
-		visit[u]=1;
-    std::cout<<u<<" ";
+    // 在v中找到了一个结点tmp 它到u中结点的距离是最短的 是min
+		u[tmp]=1;
+    std::cout<<tmp<<" ";
 		sum+=min;
-    // 重新初始化距离表 记录v->x的距离
+    // u中有新增的结点tmp了 意味着v到u最短距离有更新
 		for(int j=0;j<g.v;++j)
     {
-      // v->j
-      // v->u->j
-			if(!visit[j] && dist[j]>g.edges[u][j])
+			if(!u[j] && dist[j]>g.edges[tmp][j])
       {
-				dist[j]=g.edges[u][j];
+				dist[j]=g.edges[tmp][j];
       }
     }
 	}
