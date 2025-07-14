97327cc0e26afbfb8667eb17d143d8d3772fb59a
dingrui <Bannirui@outlook.com>
2025-07-14 12:11:21 +0800

    strlen

--- tests/test100.cpp
+++ tests/test100.cpp
@@ -1,7 +1,9 @@
+#include <cstring>
 #include <stdio.h>
 
 int main(int argc, char** argv)
 {
-    printf("hello world\n");
+    char str[] = {'h', 'e', 'l', 'l', 'o', ' ', '\0', 'w'};
+    printf("%zu\n", strlen(str));
     return 0;
 }
