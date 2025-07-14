c953bdd2e39bba233b8435d7830ee0792ed75557
dingrui <Bannirui@outlook.com>
2025-07-14 18:02:53 +0800

    运行指定ctest

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,5 @@ set -e
 
 cur_dir=$(cd $(dirname $0); pwd)
 
-# clean
-cd ${cur_dir}/build ; make clean ; make
-
 # run unit test
-cd ${cur_dir}/build/tests ; ctest -VV
+cd ${cur_dir}/build/tests ; ctest -VV -R "^test1$"
